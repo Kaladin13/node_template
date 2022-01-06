b4b12d9f89813df90526e467dae8ede9bebc83b4
kaladin <maksim.lagus@yandex.ru>
2022-01-06 08:28:13 +0300

    added nodemon and started injecting typeorm

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,54 @@
+import "reflect-metadata";
 import * as http from 'http';
 import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 import cookieParser from 'cookie-parser';
 
 import {UserController} from "./controller/user.controller";
+import {createConnection} from "typeorm";
 
-const PORT: number = 10000;
+createConnection().then(async connection => {
+
+    const PORT: number = 10000;
 
 // save cookies for 1000 days
-const timeToSave: number = 60 * 60 * 24 * 1000;
+    const timeToSave: number = 60 * 60 * 24 * 1000;
 
-const app: express.Application = express();
-const server: http.Server = http.createServer(app);
+    const app: express.Application = express();
+    const server: http.Server = http.createServer(app);
 
-app.use(session({
-    secret: uuidv4().toString(),
-    saveUninitialized:true,
-    cookie: { maxAge: timeToSave },
-    resave: false
-}));
-app.use(cookieParser());
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true }));
+    app.use(session({
+        secret: uuidv4().toString(),
+        saveUninitialized: true,
+        cookie: {maxAge: timeToSave},
+        resave: false
+    }));
+    app.use(cookieParser());
+
+//enable cors-policy
+    app.use(cors());
+
+// enable json-parse middleware
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
 
 
-app.get('/', (req: express.Request, res: express.Response) : void => {
-    console.log(req.session);
-    if (req.session.id){
-        console.log(`Success ${req.session.id}`)
-    }
-    res.status(200).send('Okay');
-})
+    app.get('/', (req: express.Request, res: express.Response): void => {
+        console.log(req.session);
+        if (req.session.id) {
+            console.log(`Success ${req.session.id}`)
+        }
+        res.status(200).send('Okay');
+    })
 
-const cont = new UserController();
+    const cont = new UserController();
 
-app.post('/us', cont.createUser);
+    app.post('/us', cont.createUser);
 
-server.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-})
+    server.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`)
+    })
+
+}).catch(error => console.log("TypeORM connection error with database: ", error));
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -6,7 +6,7 @@ export class UserController {
 
     async createUser(req: express.Request, res: express.Response) {
         console.log(req.body);
-        console.log(req.query);
+        //console.log(req.query);
         const userDto: UserDtoModel = { login: req.body.login, password: req.body.password};
         console.log(userDto);
         res.status(200).send(JSON.stringify(userDto));
